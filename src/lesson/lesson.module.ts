fd9284ce9a9fcdf400fab3b57796afe5facdeea8
Seme30 <semahegn.adugna1@gmail.com>
2024-03-27 12:20:59 +0300

    added service to providers

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
+import { LessonService } from './lesson.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Lesson]),
     ],
-    providers: [LessonResolver]
+    providers: [LessonResolver, LessonService]
 })
 export class LessonModule {}
