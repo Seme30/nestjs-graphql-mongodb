eb2f63619c5df564ccb6fac6c006acc84e6623a6
Seme30 <semahegn.adugna1@gmail.com>
2024-03-27 12:46:26 +0300

    added lessons method to return lessons array

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Lesson } from "./lesson.entity";
 import { Repository } from "typeorm";
 import { v4 as uuid } from 'uuid'
+import { CreateLessonInput } from "./lesson.input";
 
 @Injectable()
 export class LessonService{
@@ -12,11 +13,15 @@ export class LessonService{
     ){}
 
     async getLesson(id: string): Promise<Lesson> {
-        console.log((await this.lessonRepository.findOne({ where: { id } })))
         return this.lessonRepository.findOne({ where: { id } });
     }
 
-    async createLesson(name:string, startDate:string, endDate:string): Promise<Lesson>{
+    async getLessons(): Promise<Lesson[]> {
+        return this.lessonRepository.find();
+    }    
+
+    async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson>{
+        const {name, startDate, endDate} = createLessonInput
         const lesson = this.lessonRepository.create({
             id: uuid(),
             name,
